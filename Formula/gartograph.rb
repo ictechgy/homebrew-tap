81ba5b8c930bf4d900ff344994caaa4132d28583
Coden <ictechgy@gmail.com>
2026-09-23 12:14:22 +0900

    gartograph 0.5.0

--- Formula/gartograph.rb
+++ Formula/gartograph.rb
@@ -6,26 +6,26 @@
 class Gartograph < Formula
   desc "Queryable dependency graph for Go codebases, built on go/packages"
   homepage "https://github.com/ictechgy/gartograph"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.4.0/gartograph-0.4.0-darwin-arm64.tar.gz"
-      sha256 "19669eda30dccf6e711980361da0faf3d00d33d1510cc002e0f5e1120e81e411"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.5.0/gartograph-0.5.0-darwin-arm64.tar.gz"
+      sha256 "f98a04a42ac1cde4e403c8463f3dbcac3d3589dea0252b71793d54a4da7040bb"
     else
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.4.0/gartograph-0.4.0-darwin-amd64.tar.gz"
-      sha256 "87ea76c4c563c713a447569dbe986ae89f7ba2b885db39b9604d60bd0992e38a"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.5.0/gartograph-0.5.0-darwin-amd64.tar.gz"
+      sha256 "49863da3092255be971eb0396f7f246a10851054b48fe3f8d47519255ca0cf56"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.4.0/gartograph-0.4.0-linux-arm64.tar.gz"
-      sha256 "bd6c99be31d0c3a58a6c925f47814335a69f618aabbd20026a41e12893792beb"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.5.0/gartograph-0.5.0-linux-arm64.tar.gz"
+      sha256 "7527bce098ce50d88f990053800a7db6ee70e0ae4b0699d70e5af5383767fec2"
     else
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.4.0/gartograph-0.4.0-linux-amd64.tar.gz"
-      sha256 "0935a545ce1ffb974d9555be4c4bb943b699e70c4d574601c43638e1757ea588"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.5.0/gartograph-0.5.0-linux-amd64.tar.gz"
+      sha256 "1f00eb9dcaab9d003dd83aa6da64496dd1a74b3895188c648f9868386f6a461e"
     end
   end
 
