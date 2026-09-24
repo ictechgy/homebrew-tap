59fbe781a188e808d88620cbe99a422c0b831fcf
Coden <ictechgy@gmail.com>
2026-09-24 13:54:47 +0900

    gartograph 0.7.0

--- Formula/gartograph.rb
+++ Formula/gartograph.rb
@@ -6,26 +6,26 @@
 class Gartograph < Formula
   desc "Queryable dependency graph for Go codebases, built on go/packages"
   homepage "https://github.com/ictechgy/gartograph"
-  version "0.6.0"
+  version "0.7.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.6.0/gartograph-0.6.0-darwin-arm64.tar.gz"
-      sha256 "767c846bc314d6ec0f4a01253a65efd51547e158ed59a83680d429be3b3c5db9"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.7.0/gartograph-0.7.0-darwin-arm64.tar.gz"
+      sha256 "7bd0b47d430f7f3daf3e3ec8264b451217856b61e08a8de3b025791949fc1c15"
     else
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.6.0/gartograph-0.6.0-darwin-amd64.tar.gz"
-      sha256 "4835e17061756f3347490987b0f6caf1bf32baf66edbd43dcf0307455f2b9536"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.7.0/gartograph-0.7.0-darwin-amd64.tar.gz"
+      sha256 "62e072d905828fbcb92680c68e961acd2c2a1845540e4f530dcc27c8e5234971"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.6.0/gartograph-0.6.0-linux-arm64.tar.gz"
-      sha256 "ce7bb02df5f579d2a49b5fc4a69c7937d7e8a63e5e6005a048737950131b18f3"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.7.0/gartograph-0.7.0-linux-arm64.tar.gz"
+      sha256 "1056d2fa58009884498767930fcfe9b7f2b74db41cb54e5102270dd3e5df2d68"
     else
-      url "https://github.com/ictechgy/gartograph/releases/download/v0.6.0/gartograph-0.6.0-linux-amd64.tar.gz"
-      sha256 "4f7a34ae3dbdc3c1afed500020871f00e5e12ea032a56218c57c39d8162eb42a"
+      url "https://github.com/ictechgy/gartograph/releases/download/v0.7.0/gartograph-0.7.0-linux-amd64.tar.gz"
+      sha256 "caf316404bbf7024f79f8b4afc7a59de2c7820791f0aece19dcc190fb63d5e0b"
     end
   end
 
